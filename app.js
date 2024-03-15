b1af5e3dcaf32d9d24b96b78a3aae16d007b60e0
Harishbpatil <hp777489@gmail.com>
2024-03-15 09:13:49 +0530

    create a user table

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const path = require("path");
+
 require("dotenv").config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-// const User = require('./models/user');
+const User = require("./models/user");
 
 const app = express();
 
@@ -18,14 +20,14 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User.findById('5baa2528563f16379fc8a610')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("5bab316ce0a7c75f783cb8a8")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -33,8 +35,24 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect("process.env.MONGODB_URI")
-  .then((result) => {
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    return User.findOne();
+  })
+  .then((user) => {
+    if (!user) {
+      const newUser = new User({
+        name: "harish",
+        email: "hp777489.com",
+        cart: {
+          items: [],
+        },
+      });
+      return newUser.save();
+    }
+    return user;
+  })
+  .then(() => {
     app.listen(3000);
   })
   .catch((err) => {
